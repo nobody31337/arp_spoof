a014231c661ab6cb95a71a3c6cefe77678359f50
Young-woong Lazarus Sohn <44917049+syw0919@users.noreply.github.com>
2020-05-09 19:54:02 +0900

    Organizing the comments

--- protocol/ethernet.h
+++ protocol/ethernet.h
@@ -8,16 +8,16 @@
 #define ETH_ALEN	6
 
 #define	ETHERTYPE_PUP		0x0200		/* Xerox PUP 				*/
-#define ETHERTYPE_SPRITE	0x0500		/* Sprite 					*/
-#define	ETHERTYPE_IP		0x0800		/* IP 						*/
-#define	ETHERTYPE_ARP		0x0806		/* Address resolution 		*/
+#define ETHERTYPE_SPRITE	0x0500		/* Sprite 				*/
+#define	ETHERTYPE_IP		0x0800		/* IP 					*/
+#define	ETHERTYPE_ARP		0x0806		/* Address resolution 			*/
 #define	ETHERTYPE_REVARP	0x8035		/* Reverse ARP 				*/
-#define ETHERTYPE_AT		0x809B		/* AppleTalk protocol 		*/
+#define ETHERTYPE_AT		0x809B		/* AppleTalk protocol 			*/
 #define ETHERTYPE_AARP		0x80F3		/* AppleTalk ARP 			*/
-#define	ETHERTYPE_VLAN		0x8100		/* IEEE 802.1Q VLAN tagging */
-#define ETHERTYPE_IPX		0x8137		/* IPX 						*/
-#define	ETHERTYPE_IPV6		0x86dd		/* IP protocol version 6 	*/
-#define ETHERTYPE_LOOPBACK	0x9000		/* used to test interfaces 	*/
+#define	ETHERTYPE_VLAN		0x8100		/* IEEE 802.1Q VLAN tagging		*/
+#define ETHERTYPE_IPX		0x8137		/* IPX 					*/
+#define	ETHERTYPE_IPV6		0x86dd		/* IP protocol version 6 		*/
+#define ETHERTYPE_LOOPBACK	0x9000		/* used to test interfaces 		*/
 
 struct mac_addr{
 	uint8_t oui[3];
